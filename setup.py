4677d55932accd42bfea3d3c0874a36a5a6d18d9
Bohan Liu <bohan3@illinois.edu>
2024-10-29 22:13:14 -0400

    fix

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
-from setuptools import setup
+from setuptools import setup, find_packages()
 
 setup(
     name="saycam_models",
     version="1.0",
     author="Emin Orhan",
     description="Model definition and utilities from silicon-menagerie",
+    packages=find_packages(),
     py_modules=["saycam_utils"],
     python_requires=">=3.9",
     license="MIT",
